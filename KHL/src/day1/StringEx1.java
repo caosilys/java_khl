d1d981880c9f6ee82c49663ef1c5723596edc8a5
caosilys <93233391+caosilys@users.noreply.github.com>
2021-10-27 16:38:05 +0900

    Create StringEx1.java

---
+++ KHL/src/day1/StringEx1.java
@@ -0,0 +1,17 @@
+package day1;
+
+public class StringEx1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String str1;
+		String str2 = "Hello";
+		
+		System.out.println(str2);
+		
+		System.out.println(str2+ " world!");
+		
+	}
+
+}
