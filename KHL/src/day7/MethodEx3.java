d8450baa8f318c63e3099d2a979c51de96e37bf3
caosilys <93233391+caosilys@users.noreply.github.com>
2021-11-04 17:06:32 +0900

    day7

---
+++ KHL/src/day7/MethodEx3.java
@@ -0,0 +1,27 @@
+package day7;
+
+import java.util.Arrays;
+
+public class MethodEx3 {
+
+	public static void main(String[] args) {
+		/* 참조변수를 매개변수로 설정하면 원본이 바뀜		 * */
+		int test[] = {1, 2, 3, 4};
+		
+		System.out.println(Arrays.toString(test));
+		
+		trans(test);
+		
+		System.out.println(Arrays.toString(test));
+		
+	}
+	
+	public static void trans(int[] nomal)
+	{
+		for(int i = 0 ; i < nomal.length ; i++)
+		{
+			nomal[i] = 0;
+		}
+	}
+
+}
